a64793a395571ad06de76c3f0c44545ccb510bf9
ado sir <1334435738@qq.com>
2018-12-24 11:41:27 +0800

    置顶

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 DOCKER_ID=docker_phpfpm_1
+DOCKER_ID2=docker_phpfpm2_1
 docker-compose up -d
 docker exec -i $DOCKER_ID /bin/bash -c 'supervisord -c /etc/supervisor/conf.d/tpqueue.conf'
+docker exec -i $DOCKER_ID2 /bin/bash -c 'supervisord -c /etc/supervisor/conf.d/tpqueue.conf'
+docker exec -i $DOCKER_ID /bin/bash -c 'supervisord -c /etc/supervisor/conf.d/luckqueue.conf'
